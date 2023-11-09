8702f7e614356c89f151e8a451b1af00ef86bc1c
Ivan Genik <IVGenik@rambler.ru>
2023-11-09 19:13:28 +0500

    Add files via upload

---
+++ Russia Monetary base (in a narrow definition) weekly/Russia Monetary base (in a narrow definition) weekly.py
@@ -0,0 +1,47 @@
+'''
+Input and visualisation from xlsx
+Russia Monetary base (in a narrow definition) weekly
+'''
+
+import os
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_string_dtype
+import matplotlib.pyplot as plt
+
+the_title = 'Russia Monetary base (in a narrow definition) weekly'
+
+def print_row_col(dat: pd.DataFrame, title: str, view_head_tile: bool = False):
+    print()
+    print(title)
+    print('num rows    = ', dat.shape[0]) #, ' ', dat.axes[0])
+    print('num cols = ', dat.shape[1]) #, ' ', dat.axes[1])
+    if view_head_tile:
+        print(dat.head(7));        print(dat.tail(7))
+    print()
+
+#-(1) -- input
+main_xls_fname = the_title+'.xlsx'
+mb_DataFrame = pd.read_excel(main_xls_fname, sheet_name='Data')
+print_row_col(mb_DataFrame, 'before-'+the_title, True)
+print(mb_DataFrame.columns)
+
+#-(2) -- columns name
+print('col1 = ', mb_DataFrame.columns[0])
+print('col2 = ', mb_DataFrame.columns[1])
+col_name = [mb_DataFrame.columns[0], mb_DataFrame.columns[1]]
+#-(3) -- 1 col to date
+mb_DataFrame[col_name[0]] = mb_DataFrame[col_name[0]].astype(str)
+# сменить тип ячеек столбца со строкового на дату https://stackoverflow.com/questions/17134716/convert-dataframe-column-type-from-string-to-datetime
+mb_DataFrame[col_name[0]] = pd.to_datetime(mb_DataFrame[col_name[0]], format="%Y-%m-%d")
+x = mb_DataFrame[col_name[0]];
+y = mb_DataFrame[col_name[1]]
+
+fig, ax = plt.subplots(figsize=(10, 8))
+# ax.xaxis.set_major_locator(plt.MultipleLocator(2))
+plt.title(the_title)
+plt.plot(x,y)
+plt.xlabel(col_name[0])
+plt.ylabel(col_name[1])
+plt.grid()
+plt.show()
